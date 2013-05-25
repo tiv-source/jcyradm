24818ada82efa99448d68beee19a0a07c52e2f5e
tiv-source <opensource@tiv-consulting.de>
2013-05-25 18:44:48 +0200

    CLOSED - task 8: Add basic class to repository http://github.com/tiv-source/jcyradm/issues/issue/8

---
+++ src/main/java/de/nethold/lib/jcyradm/JCyrAdm.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package de.nethold.lib.jcyradm;
+
+/**
+ * JCyrAdm ist eine Libary die dazu dient eine Verbindung mit einem 
+ * Cyrus-Imap-Server herzustellen und um dann Verwaltungsoperationen 
+ * auszuführen (createMailbox, removeMailbox, etc. ).
+ *
+ * @author Marc Michele
+ *
+ */
+public class JCyrAdm {
+
+}
