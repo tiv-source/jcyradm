13fe177083fd115aae64defec56e4437c69a3cbd
tiv-source <opensource@tiv-consulting.de>
2013-05-26 06:44:26 +0200

    OPEN - task 13: Add exception classes to repository http://github.com/tiv-source/jcyradm/issues/issue/13

---
+++ src/main/java/de/nethold/lib/jcyradm/exception/NoMailbox.java
@@ -0,0 +1,24 @@
+package de.nethold.lib.jcyradm.exception;
+
+/**
+ * Die Klasse NoMailbox ist die Ausnahme die geworfen wird, wenn es die benutze
+ * Mailbox nicht gibt.
+ *
+ * @author Marc Michele
+ *
+ */
+public class NoMailbox extends Exception {
+
+    /**
+     * SerialVersionUID der Klasse NoMailbox.
+     */
+    private static final long serialVersionUID = -642162588858173904L;
+
+    /**
+     * Konstruktor der Klasse NoMailbox.
+     */
+    public NoMailbox() {
+        super("Mailbox not exists");
+    }
+
+} // Ende class
