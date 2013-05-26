415113807c5d8907026c89bb4e92248e29740e2e
tiv-source <opensource@tiv-consulting.de>
2013-05-26 05:37:40 +0200

    OPEN - task 13: Add exception classes to repository http://github.com/tiv-source/jcyradm/issues/issue/13

---
+++ src/main/java/de/nethold/lib/jcyradm/exception/AuthenticationFailure.java
@@ -0,0 +1,24 @@
+package de.nethold.lib.jcyradm.exception;
+
+/**
+ * Die Klasse AuthenticationFailure ist die Ausnahme die geworfen wird wenn der
+ * Benutzername oder das Passwort nicht gültig sind.
+ *
+ * @author Marc Michele
+ *
+ */
+public class AuthenticationFailure extends Exception {
+
+    /**
+     * SerialVersionUID der Klasse AuthenticationFailure.
+     */
+    private static final long serialVersionUID = 5655198435093474377L;
+
+    /**
+     * Konstruktor der Klasse AuthenticationFailure.
+     */
+    public AuthenticationFailure() {
+        super("Wrong User or Password.");
+    }
+
+} // End class
