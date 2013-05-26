8baac743bf9f1aef7facc95492ffc810bee0785f
tiv-source <opensource@tiv-consulting.de>
2013-05-26 11:46:32 +0200

    OPEN - task 13: Add exception classes to repository http://github.com/tiv-source/jcyradm/issues/issue/13
    Add new class

---
+++ src/main/java/de/nethold/lib/jcyradm/exception/UnexpectedExtraArguments.java
@@ -0,0 +1,25 @@
+package de.nethold.lib.jcyradm.exception;
+
+/**
+ * Die Klasse UnexpectedExtraArguments ist eine Exception die geworfen wird,
+ * wenn an den benutzen Server ein Kommando mit einem zusätzlichen Parameter
+ * gesendet wird das der Server nicht kennt.
+ *
+ * @author Marc Michele
+ *
+ */
+public class UnexpectedExtraArguments extends Exception {
+
+    /**
+     * SerialVersionUID der Klasse UnexpectedExtraArguments.
+     */
+    private static final long serialVersionUID = 2801608929489543738L;
+
+    /**
+     * Konstruktor der Klasse UnexpectedExtraArguments.
+     */
+    public UnexpectedExtraArguments() {
+        super("Unexpected Extra Arguments");
+    }
+
+} // Ende class
