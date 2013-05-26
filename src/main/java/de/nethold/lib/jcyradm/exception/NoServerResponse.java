ed4c345f903b46bf723544d2f91136eb8399ec62
tiv-source <opensource@tiv-consulting.de>
2013-05-26 10:48:35 +0200

    OPEN - task 13: Add exception classes to repository http://github.com/tiv-source/jcyradm/issues/issue/13

---
+++ src/main/java/de/nethold/lib/jcyradm/exception/NoServerResponse.java
@@ -0,0 +1,24 @@
+package de.nethold.lib.jcyradm.exception;
+
+/**
+ * Die Klasse NoServerResponse ist die Ausnahme die geworfen wird, wenn von dem
+ * benutzen Server keine Daten empfangen werden.
+ *
+ * @author Marc Michele
+ *
+ */
+public class NoServerResponse extends Exception {
+
+    /**
+     * SerialVersionUID der Klasse NoServerResponse.
+     */
+    private static final long serialVersionUID = -5297334996803926809L;
+
+    /**
+     * Konstruktor der Klasse NoServerResponse.
+     */
+    public NoServerResponse() {
+        super("No Server Response.");
+    }
+
+} // Ende class
