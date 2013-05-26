09634484ca1f94713999b679f0db934e89215214
tiv-source <opensource@tiv-consulting.de>
2013-05-26 09:35:04 +0200

    OPEN - task 13: Add exception classes to repository http://github.com/tiv-source/jcyradm/issues/issue/13

---
+++ src/main/java/de/nethold/lib/jcyradm/exception/NoQuota.java
@@ -0,0 +1,24 @@
+package de.nethold.lib.jcyradm.exception;
+
+/**
+ * Die Klasse NoQuota ist die Ausnahme die geworfen wird, wenn für die
+ * angegebene Mailbox keine Quota angelegt wurde.
+ *
+ * @author Marc Michele
+ *
+ */
+public class NoQuota extends Exception {
+
+    /**
+     * SerialVersionUID der Klasse NoQuota.
+     */
+	private static final long serialVersionUID = -3305938792286503828L;
+
+	/**
+     * Konstruktor der Klasse NoQuota.
+     */
+    public NoQuota() {
+        super("Mailbox not exists");
+    }
+
+} // Ende class
