dd1538ae2093c52edbd3f9861adfc08bd86829c7
tiv-source <marc.michele@tiv-source.de>
2013-08-20 04:45:06 +0200

    Fix checkstyle errors.

--- src/main/java/de/tivsource/lib/jcyradm/exception/AuthenticationFailure.java
+++ src/main/java/de/tivsource/lib/jcyradm/exception/AuthenticationFailure.java
@@ -12,9 +12,9 @@ public class AuthenticationFailure extends Exception {
     /**
      * SerialVersionUID der Klasse AuthenticationFailure.
      */
-	private static final long serialVersionUID = 5655198435093474377L;
+    private static final long serialVersionUID = 5655198435093474377L;
 
-	/**
+    /**
      * Konstruktor der Klasse AuthenticationFailure.
      */
     public AuthenticationFailure() {
